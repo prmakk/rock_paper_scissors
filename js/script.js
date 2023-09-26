1d9b428472b5dd9edaf748347f142a50c1242a92
prmk <priymakvlad420@gmail.com>
2023-09-26 11:34:58 +0300

    final result + responsive dsgn

--- js/script.js
+++ js/script.js
@@ -1,6 +1,18 @@
+//PLAYER
 const playerRock = document.querySelector('.game__player-rock');
 const playerPaper = document.querySelector('.game__player-paper');
 const playerScissors = document.querySelector('.game__player-scissors');
+//PC
+const pcRock = document.querySelector('.pc_rock');
+const pcPaper = document.querySelector('.pc_paper');
+const pcScissors = document.querySelector('.pc_scissors');
+const pcChose = document.querySelector('.pc_chose');
+
+const result = document.querySelector('.result');
+const score = document.querySelector('.scoreTitle');
+const newGameBtn = document.querySelector('.new_game_btn');
+
+let count = 0;
 
 checkWinner();
 const opponentArr = ['rock', 'paper', 'scissors'];
@@ -9,24 +21,199 @@ function randomInteger(min, max) {
     // случайное число от min до (max+1)
     let rand = min + Math.random() * (max + 1 - min);
     return Math.floor(rand);
-  }
+}
 
 function checkWinner(){
     if(playerRock.onclick = () =>{
-        if(opponentArr[randomInteger(0,3)] == 'rock') console.log('R Draw R');
-        else if (opponentArr[randomInteger(0,3)] == 'paper') console.log('R Lose P');
-        else console.log('R Win S');
+        clearPC();
+        if(opponentArr[randomInteger(0,3)] == 'rock') {
+            pcChose.style = 'display: block';
+            pcRock.style = 'display: block';
+            result.style = 'display: block';
+            newGameBtn.style = 'display: block';
+
+            pcChose.innerHTML = 'PC picked a rock';
+            result.innerHTML = 'Draw!';
+            banPlayerClicks();
+
+            newGameBtn.onclick = () =>{
+                allowPlayerClicks();
+                clearPC();
+            }
+
+            count += 0;
+            score.innerHTML = count;
+        }
+        else if (opponentArr[randomInteger(0,3)] == 'paper'){
+            pcChose.style = 'display: block';
+            pcPaper.style = 'display: block';
+            result.style = 'display: block';
+            newGameBtn.style = 'display: block';
+
+            pcChose.innerHTML = 'PC picked a paper';
+            result.innerHTML = 'Lose!';
+            banPlayerClicks();
+
+            newGameBtn.onclick = () =>{
+                allowPlayerClicks();
+                clearPC();
+            }
+
+            count -= 1;
+            score.innerHTML = count;
+        }
+        else {
+            pcChose.style = 'display: block';
+            pcScissors.style = 'display: block';
+            result.style = 'display: block';
+            newGameBtn.style = 'display: block';
+
+            pcChose.innerHTML = 'PC picked a scissors';
+            result.innerHTML = 'Win!';
+            banPlayerClicks();
+
+            newGameBtn.onclick = () =>{
+                allowPlayerClicks();
+                clearPC();
+            }
+
+            count += 1;
+            score.innerHTML = count;
+        }
     });
 
     if(playerScissors.onclick = () =>{
-        if(opponentArr[randomInteger(0,3)] == 'rock') console.log('S Lose R');
-        else if (opponentArr[randomInteger(0,3)] == 'paper') console.log('S Win P');
-        else console.log('S Draw S');
+        if(opponentArr[randomInteger(0,3)] == 'rock'){
+            pcChose.style = 'display: block';
+            pcRock.style = 'display: block';
+            result.style = 'display: block';
+            newGameBtn.style = 'display: block';
+
+            pcChose.innerHTML = 'PC picked a rock';
+            result.innerHTML = 'Lose!';
+            banPlayerClicks();
+
+            newGameBtn.onclick = () =>{
+                allowPlayerClicks();
+                clearPC();
+            }
+
+            count -= 1;
+            score.innerHTML = count;
+        }
+        else if (opponentArr[randomInteger(0,3)] == 'paper'){
+            pcChose.style = 'display: block';
+            pcPaper.style = 'display: block';
+            result.style = 'display: block';
+            newGameBtn.style = 'display: block';
+
+            pcChose.innerHTML = 'PC picked a paper';
+            result.innerHTML = 'Win!';
+            banPlayerClicks();
+
+            newGameBtn.onclick = () =>{
+                allowPlayerClicks();
+                clearPC();
+            }
+
+            count += 1;
+            score.innerHTML = count;
+        }
+        else{
+            pcChose.style = 'display: block';
+            pcScissors.style = 'display: block';
+            result.style = 'display: block';
+            newGameBtn.style = 'display: block';
+
+            pcChose.innerHTML = 'PC picked a scissors';
+            result.innerHTML = 'Draw!';
+            banPlayerClicks();
+
+            newGameBtn.onclick = () =>{
+                allowPlayerClicks();
+                clearPC();
+            }
+
+            count += 0;
+            score.innerHTML = count;
+        }
     });
     
     if(playerPaper.onclick = () =>{
-        if(opponentArr[randomInteger(0,3)] == 'rock') console.log('P Win R');
-        else if (opponentArr[randomInteger(0,3)] == 'paper') console.log('P Draw P');
-        else console.log('P Lose S');
+        if(opponentArr[randomInteger(0,3)] == 'rock'){
+            pcChose.style = 'display: block';
+            pcRock.style = 'display: block';
+            result.style = 'display: block';
+            newGameBtn.style = 'display: block';
+
+            pcChose.innerHTML = 'PC picked a rock';
+            result.innerHTML = 'Win!';
+            banPlayerClicks();
+
+            newGameBtn.onclick = () =>{
+                allowPlayerClicks();
+                clearPC();
+            }
+
+            count += 1;
+            score.innerHTML = count;
+        }
+        else if (opponentArr[randomInteger(0,3)] == 'paper'){
+            pcChose.style = 'display: block';
+            pcPaper.style = 'display: block';
+            result.style = 'display: block';
+            newGameBtn.style = 'display: block';
+
+            pcChose.innerHTML = 'PC picked a paper';
+            result.innerHTML = 'Draw!';
+            banPlayerClicks();
+
+            newGameBtn.onclick = () =>{
+                allowPlayerClicks();
+                clearPC();
+            }
+
+            count += 0;
+            score.innerHTML = count;
+        }
+        else {
+            pcChose.style = 'display: block';
+            pcScissors.style = 'display: block';
+            result.style = 'display: block';
+            newGameBtn.style = 'display: block';
+
+            pcChose.innerHTML = 'PC picked a scissors';
+            result.innerHTML = 'Lose!';
+            banPlayerClicks();
+
+            newGameBtn.onclick = () =>{
+                allowPlayerClicks();
+                clearPC();
+            }
+
+            count -= 1;
+            score.innerHTML = count;
+        }
     });
+}
+
+function clearPC(){
+    pcRock.style = 'display: none';
+    pcPaper.style = 'display: none';
+    pcScissors.style = 'display: none';
+    pcChose.style = 'display: none';
+    result.style = 'display: none';
+    newGameBtn.style = 'display: none';
+}
+
+function banPlayerClicks(){
+    playerRock.style.pointerEvents = 'none';
+    playerPaper.style.pointerEvents = 'none';
+    playerScissors.style.pointerEvents = 'none';
+}
+
+function allowPlayerClicks(){
+    playerRock.style.pointerEvents = 'auto';
+    playerPaper.style.pointerEvents = 'auto';
+    playerScissors.style.pointerEvents = 'auto';
 }
